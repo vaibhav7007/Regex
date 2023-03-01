78ad573abc479534cae277ba7c8e93e190fca3b5
vaibhav <vaibhavmehta524@gmail.com>
2023-03-01 08:33:34 +0530

    Modified to Valid First Name Program

---
+++ Regex-Problems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Regex_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           RegexProblem regex = new RegexProblem();
+            regex.ValidFirstName("Vaibhav");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Regex-Problems/Regex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Problems
+{
+    public class RegexProblem
+    {
+        public void ValidFirstName(string name)
+        {
+            string firstName = "^[A - Z][a - z]{ 3,}?";
+            if (Regex.IsMatch(name, firstName))
+            {
+                Console.WriteLine("First name is matching with Regex");
+            }
+            else
+            {
+                Console.WriteLine("First name is not matching with Regex");
+            }
+        }
+    }
+}
+                
